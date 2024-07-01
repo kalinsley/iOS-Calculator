91dec416c13b1d507ced83ce251715ac2f14ba63
Kai Linsley <98914767+kalinsley@users.noreply.github.com>
2024-07-01 20:56:32 +0900

    Add files via upload

---
+++ CalculatorTutorial/CalculatorTutorial/CalculatorTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTutorialApp.swift
+//  CalculatorTutorial
+//
+//  Created by Kai Linsley on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
